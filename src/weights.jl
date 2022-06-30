8f027c04e255bc626d75a3c87159f96ede6164bf
Omkar A. Katta <omkarakatta@gmail.com>
2022-06-30 10:54:43 -0400

    feat(weights): compute average weights for LATE

---
+++ src/weights.jl
@@ -0,0 +1,29 @@
+################################################################################
+# Goal: compute the average (across instruments) weights on MTRs
+#
+# The average weights are piecewise-constant functions of u.
+# The `compute_average_weights` function will return a data frame that stores
+# value of the average weight at the endpoints of each piece.
+# Hence, there will be an even number of rows. There will be 3 columns:
+#   1. value of u
+#   2. average weight for d = 1
+#   3. average weight for d = 0
+#
+# The average weights for IV-like estimands rely on the s(d, z) function.
+# The average weights for target parameters are derived manually.
+#
+################################################################################
+
+function compute_average_weights(tp::TargetParameter)
+    if tp.name == "LATE(u₁, u₂)"
+        u₁ = tp.int_limits(1)[1] # only coded 1-dim case
+        u₂ = tp.int_limits(1)[2] # only coded 1-dim case
+        results = DataFrame(u = [u₁, u₂])
+        results[:, "average weight for d = 1"] .= 1 / (u₂ - u₁)
+        results[:, "average weight for d = 0"] = -results[:, 2]
+    else
+        print("Weight Computation is unsupported for this target parameter.")
+    end
+    return results
+end
+export compute_average_weights
