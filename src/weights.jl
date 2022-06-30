1c010a6d75ebf1b87be2c5809bfac8205106d907
Omkar A. Katta <omkarakatta@gmail.com>
2022-06-30 14:57:16 -0400

    chore(weights): clean up comments

--- src/weights.jl
+++ src/weights.jl
@@ -3,8 +3,7 @@
 #
 # The average weights are piecewise-constant functions of u.
 # The `compute_average_weights` function will return a data frame that stores
-# value of the average weight at the endpoints of each piece.
-# Hence, there will be an even number of rows. There will be 3 columns:
+# value of the average weight at the endpoints of each nontrivial piece.
 #   1. value of u
 #   2. average weight for d = 1
 #   3. average weight for d = 0
@@ -16,8 +15,8 @@
 
 function compute_average_weights(tp::TargetParameter)
     if tp.name == "LATE(u₁, u₂)"
-        u₁ = tp.int_limits(1)[1] # only coded 1-dim case
-        u₂ = tp.int_limits(1)[2] # only coded 1-dim case
+        u₁ = tp.int_limits(1)[1] # only coded the case of 1 instrument
+        u₂ = tp.int_limits(1)[2] # only coded the case of 1 instrument
         results = DataFrame(u = [u₁, u₂])
         results[:, "average weight for d = 1"] .= 1 / (u₂ - u₁)
         results[:, "average weight for d = 0"] = -results[:, 2]
